4d07299c3f25dd0b9187108c520fe27174ac69d8
Nikita Lukjanov <nlukjanov@gmail.com>
2020-03-18 17:41:52 +0300

    add intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
 
+import 'bulma'
+
+import Intro from './components/Intro'
+import Projects from './components/Projects'
+
 function App() {
   return (
     <>
-      <div>
-        hello
-      </div>
+      <Intro />
+      <Projects />
     </>
   )
 }
