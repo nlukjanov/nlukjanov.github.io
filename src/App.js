3121f54b1593a4676dbc3bea8f62b5469f0fcc2c
Nikita Lukjanov <nlukjanov@gmail.com>
2020-03-18 19:26:49 +0300

    import and style

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import 'bulma'
 
 import Intro from './components/Intro'
 import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <>
       <Intro />
       <Projects />
+      <div className='section hero is-fullheight'>
+        <Skills />
+        <Contact />
+      </div>
     </>
   )
 }
