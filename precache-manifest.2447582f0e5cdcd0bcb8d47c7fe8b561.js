d98031b82762e3fceecbe73294af7375131815b0
Nikita Lukjanov <nlukjanov@gmail.com>
2020-03-19 11:55:00 +0300

    Updates

---
+++ precache-manifest.2447582f0e5cdcd0bcb8d47c7fe8b561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5eb1d487da35cb138a2985b36a2c4d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b30a594c8121c09aad62",
+    "url": "/static/css/2.1c2325a6.chunk.css"
+  },
+  {
+    "revision": "a64e7f8e2a020213bf3a",
+    "url": "/static/css/main.e7a6dcc1.chunk.css"
+  },
+  {
+    "revision": "b30a594c8121c09aad62",
+    "url": "/static/js/2.cd074dec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cd074dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64e7f8e2a020213bf3a",
+    "url": "/static/js/main.6b9b5963.chunk.js"
+  },
+  {
+    "revision": "2ab5528f29807ec3af41",
+    "url": "/static/js/runtime-main.8b054bcd.js"
+  }
+]);
