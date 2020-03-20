2f41a6efa23244043b6e93d306d03f74ce119e64
Nikita Lukjanov <nlukjanov@gmail.com>
2020-03-20 15:09:59 +0300

    style project tiles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,8 +22,6 @@ const Projects = () => {
   //   return
   // }
 
-
-
   return (
     <section className='section hero is-fullheight' id='projects'>
       <div className='title'>Projects</div>
@@ -34,7 +32,8 @@ const Projects = () => {
           aria-label='pagination'
         >
           {/* <a href='!#' className={'arrow level-item is-centered is-size-5 ' + `${project === 1 ? '' : 'active'}`} onClick={handlePrevious}><i className="fas fa-angle-left"></i></a> */}
-          <button className={
+          <button
+            className={
               'pagenum level-item is-centered is-size-5 is-family-secondary ' +
               `${project === 1 ? 'current' : ''}`
             }
@@ -44,7 +43,8 @@ const Projects = () => {
           >
             1
           </button>
-          <button className={
+          <button
+            className={
               'pagenum level-item is-centered is-size-5 is-family-secondary ' +
               `${project === 2 ? 'current' : ''}`
             }
@@ -54,7 +54,8 @@ const Projects = () => {
           >
             2
           </button>
-          <button className={
+          <button
+            className={
               'pagenum level-item is-centered is-size-5 is-family-secondary ' +
               `${project === 3 ? 'current' : ''}`
             }
@@ -64,7 +65,8 @@ const Projects = () => {
           >
             3
           </button>
-          <button className={
+          <button
+            className={
               'pagenum level-item is-centered is-size-5 is-family-secondary ' +
               `${project === 4 ? 'current' : ''}`
             }
@@ -74,7 +76,8 @@ const Projects = () => {
           >
             4
           </button>
-          <button className={
+          <button
+            className={
               'pagenum level-item is-centered is-size-5 is-family-secondary ' +
               `${project === 5 ? 'current' : ''}`
             }
@@ -84,7 +87,8 @@ const Projects = () => {
           >
             5
           </button>
-          <button className={
+          <button
+            className={
               'pagenum level-item is-centered is-size-5 is-family-secondary ' +
               `${project === 6 ? 'current' : ''}`
             }
@@ -106,52 +110,54 @@ const Projects = () => {
           </a> */}
         </nav>
       </div>
-      <div className='tile is-ancestor'>
-        <div className='tile is-vertical is-8'>
-          <div className='tile'>
-            <div className='tile is-parent is-vertical'>
-              <article className='tile is-child notification is-primary'>
-                <p className='title'>Vertical...</p>
-                <p className='subtitle'>Top tile</p>
-              </article>
-              <article className='tile is-child notification is-warning'>
-                <p className='title'>...tiles</p>
-                <p className='subtitle'>Bottom tile</p>
-              </article>
-            </div>
-            <div className='tile is-parent'>
-              <article className='tile is-child notification is-info'>
-                <p className='title'>Middle tile</p>
-                <p className='subtitle'>With an image</p>
-                <figure className='image is-4by3'>
-                  <img src='https://bulma.io/images/placeholders/640x480.png'></img>
-                </figure>
-              </article>
-            </div>
-          </div>
+      <div className='projects'>
+        <h2 className='has-text-centered is-size-2'>Project title</h2>
+        <div className='tile is-ancestor'>
           <div className='tile is-parent'>
-            <article className='tile is-child notification is-danger'>
-              <p className='title'>Wide tile</p>
-              <p className='subtitle'>Aligned with the right tile</p>
-              <div className='content'>content</div>
+            <article className='tile is-child notification card'>
+              <div className='content'>
+                <h4 className='is-size-4'>Project name</h4>
+                <p>
+                  A website to find someone to go on holiday with. It was a
+                  group project (3 people) built in 6 days using MERN stack. To
+                  maximise learning, I suggested rotating, so that each of us
+                  worked on models, controllers and front end. We spent a day
+                  planning our work and recording tasks on Trello and then went
+                  executing. I have personally built: user model, user
+                  controller, authentication, homepage component with search and
+                  filtering, my account page, set up auto-testing.
+                </p>
+              </div>
             </article>
           </div>
-        </div>
-        <div className='tile is-parent'>
-          <article className='tile is-child notification is-success'>
-            <div className='content'>
-              <p>
-                A website to find someone to go on holiday with. It was a group
-                project (3 people) built in 6 days using MERN stack. To maximise
-                learning, I suggested rotating, so that each of us worked on
-                models, controllers and front end. We spent a day planning our
-                work and recording tasks on Trello and then went executing. I
-                have personally built: user model, user controller,
-                authentication, homepage component with search and filtering, my
-                account page, set up auto-testing.
-              </p>
+          <div className='tile is-vertical is-8'>
+            <div className='tile'>
+              <div className='tile is-parent is-vertical'>
+                <article className='tile is-child notification card'>
+                  <figure className='image is-3by2'>
+                    <img src='https://bulma.io/images/placeholders/640x480.png'></img>
+                  </figure>
+                </article>
+                <div className='tile is-child notification card'>
+                  <figure className='image is-3by2'>
+                    <img src='https://bulma.io/images/placeholders/640x480.png'></img>
+                  </figure>
+                </div>
+              </div>
+              <div className='tile is-parent'>
+                <article className='tile is-child notification is-info'>
+                  <figure className='image is-9by16'>
+                    <img src='https://bulma.io/images/placeholders/640x480.png'></img>
+                  </figure>
+                </article>
+              </div>
             </div>
-          </article>
+            <div className='tile is-parent'>
+              <div className='tile is-child notification is-danger'>
+                <div className='content'>tech stack, symbols + </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
