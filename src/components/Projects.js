d069810ceae7e5be5c75faf48eb5b85450b0dce0
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-03-26 18:28:47 +0500

    add images for calendar journal

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,49 +5,41 @@ import Skills from './Skills'
 
 import tetris1 from '../images/tetris1.png'
 import tetris2 from '../images/tetris2.png'
-import tetris3 from '../images/tetris3.png'
 import artjourney1 from '../images/artjourney1.png'
 import artjourney2 from '../images/artjourney2.png'
-import artjourney3 from '../images/artjourney3.png'
 import travelr1 from '../images/travelr1.png'
 import travelr2 from '../images/travelr2.png'
-import travelr3 from '../images/travelr3.png'
 import dietlogue1 from '../images/dietlogue1.png'
 import dietlogue2 from '../images/dietlogue2.png'
-import dietlogue3 from '../images/dietlogue3.png'
 import baliskin1 from '../images/baliskin1.png'
 import baliskin2 from '../images/baliskin2.png'
-import baliskin3 from '../images/baliskin3.png'
 import rs1 from '../images/rs1.png'
 import rs2 from '../images/rs2.png'
-import rs3 from '../images/rs3.png'
+import cj1 from '../images/cj1.png'
+import cj2 from '../images/cj2.png'
 
 import tetris1small from '../images/small/tetris1.png'
 import tetris2small from '../images/small/tetris2.png'
-import tetris3small from '../images/small/tetris3.png'
 import artjourney1small from '../images/small/artjourney1.png'
 import artjourney2small from '../images/small/artjourney2.png'
-import artjourney3small from '../images/small/artjourney3.png'
 import travelr1small from '../images/small/travelr1.png'
 import travelr2small from '../images/small/travelr2.png'
-import travelr3small from '../images/small/travelr3.png'
 import dietlogue1small from '../images/small/dietlogue1.png'
 import dietlogue2small from '../images/small/dietlogue2.png'
-import dietlogue3small from '../images/small/dietlogue3.png'
 import baliskin1small from '../images/small/baliskin1.png'
 import baliskin2small from '../images/small/baliskin2.png'
-import baliskin3small from '../images/small/baliskin3.png'
 import rs1small from '../images/small/rs1.png'
 import rs2small from '../images/small/rs2.png'
-import rs3small from '../images/small/rs3.png'
+import cj1small from '../images/small/cj1.png'
+import cj2small from '../images/small/cj2.png'
 
 const projects = [
   {
     projectName: 'Dietlogue',
     deployed: 'http://bit.ly/nl-sei4-live',
     repo: 'http://bit.ly/nl-sei4-repo',
-    images: [dietlogue1, dietlogue2, dietlogue3],
-    smallImages: [dietlogue1small, dietlogue2small, dietlogue3small],
+    images: [dietlogue1, dietlogue2],
+    smallImages: [dietlogue1small, dietlogue2small],
     stack: [
       'postman',
       'devicon-git-plain',
@@ -67,8 +59,8 @@ const projects = [
     projectName: 'Travelr',
     deployed: 'http://bit.ly/nl-sei3-live',
     repo: 'http://bit.ly/nl-sei3-repo',
-    images: [travelr1, travelr2, travelr3],
-    smallImages: [travelr1small, travelr2small, travelr3small],
+    images: [travelr1, travelr2],
+    smallImages: [travelr1small, travelr2small],
     stack: [
       'devicon-git-plain',
       'npm',
@@ -90,8 +82,8 @@ const projects = [
     projectName: 'Art Journey',
     deployed: 'http://bit.ly/nl-sei2-live',
     repo: 'http://bit.ly/nl-sei2-repo',
-    images: [artjourney1, artjourney2, artjourney3],
-    smallImages: [artjourney1small, artjourney2small, artjourney3small],
+    images: [artjourney1, artjourney2],
+    smallImages: [artjourney1small, artjourney2small],
     stack: [
       'devicon-git-plain',
       'npm',
@@ -111,8 +103,8 @@ const projects = [
     projectName: 'Tetris',
     deployed: 'http://bit.ly/nl-sei1-heroku',
     repo: 'http://bit.ly/nl-sei1-repo',
-    images: [tetris1, tetris2, tetris3],
-    smallImages: [tetris1small, tetris2small, tetris3small],
+    images: [tetris1, tetris2],
+    smallImages: [tetris1small, tetris2small],
     stack: [
       'devicon-git-plain',
       'devicon-heroku-original',
@@ -128,8 +120,8 @@ const projects = [
     projectName: 'Bali Skin',
     deployed: 'http://baliskin.com/',
     repo: '',
-    images: [baliskin1, baliskin2, baliskin3],
-    smallImages: [baliskin1small, baliskin2small, baliskin3small],
+    images: [baliskin1, baliskin2],
+    smallImages: [baliskin1small, baliskin2small],
     stack: ['devicon-wordpress-plain', 'devicon-css3-plain'],
     description:
       'This is an e-commerce website that I have built while travelling. I have used WordPress themes and templates and bit of custom CSS. WordPress has a straight forward content management system which was suitable for this project.'
@@ -138,8 +130,8 @@ const projects = [
     projectName: 'Russian School',
     deployed: 'https://russianschool.org.uk/',
     repo: '',
-    images: [rs1, rs2, rs3],
-    smallImages: [rs1small, rs2small, rs3small],
+    images: [rs1, rs2],
+    smallImages: [rs1small, rs2small],
     stack: ['devicon-wordpress-plain', 'devicon-css3-plain'],
     description:
       'This is a website for Russian School in London that I was volunteering for. I have used WordPress themes and templates and a bit of custom CSS. I needed easy CMS any person could pick up in case I am not able to maintain the website anymore.WordPress seemed good fit for this purpose.'
@@ -148,8 +140,9 @@ const projects = [
     projectName: 'Calendar Journal',
     deployed: '',
     repo: '',
-    images: [],
-    stack: ['devicon-react-original'],
+    images: [cj1, cj2],
+    smallImages: [cj1small, cj2small],
+    stack: ['devicon-react-original','devicon-git-plain','devicon-visualstudio-plain'],
     description:
       'There are many sources that describe benefits of journaling. I am journaling almost every day. Currently I am working on the project where I the main page will have a calendar format with your journal entries displayed per day. It will be visually very descriptive when did you journal. Color labeling will allow for explicit visual representation of categories.'
   }
@@ -175,10 +168,10 @@ const Projects = () => {
       <div className='columns'>
         <div className='column'>
           <ModalImage
-            small={currentProject.smallImages[0]}
-            large={currentProject.images[0]}
             hideDownload={true}
             hideZoom={true}
+            small={currentProject.smallImages[0]}
+            large={currentProject.images[0]}
             alt={currentProject.projectName}
           />
         </div>
@@ -201,8 +194,8 @@ const Projects = () => {
           <ModalImage
             hideDownload={true}
             hideZoom={true}
-            small={currentProject.smallImages[2]}
-            large={currentProject.images[2]}
+            small={currentProject.smallImages[1]}
+            large={currentProject.images[1]}
             alt={currentProject.projectName}
           />
         </div>
