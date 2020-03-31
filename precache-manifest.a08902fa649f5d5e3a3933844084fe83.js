05efd5df1aba686acd3e68391856ae87c63e43f5
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-03-31 18:44:40 +0100

    Updates

---
+++ precache-manifest.a08902fa649f5d5e3a3933844084fe83.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f7b3fe57e089acc62e13a742420483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc270bae1f87443711bd",
+    "url": "/static/css/main.f2edb570.chunk.css"
+  },
+  {
+    "revision": "f0e98e0a98633ab65dbc",
+    "url": "/static/js/2.a9f827aa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a9f827aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc270bae1f87443711bd",
+    "url": "/static/js/main.37f17a5e.chunk.js"
+  },
+  {
+    "revision": "2ab5528f29807ec3af41",
+    "url": "/static/js/runtime-main.8b054bcd.js"
+  },
+  {
+    "revision": "de2202671163460143532a99cfbc351c",
+    "url": "/static/media/FourMasters-VandenKeere.de220267.woff2"
+  },
+  {
+    "revision": "9e636d7be414a4a152913e8f6c91f92b",
+    "url": "/static/media/artjourney1.9e636d7b.webp"
+  },
+  {
+    "revision": "a3868b90ee6e611f38e2c232554468bf",
+    "url": "/static/media/artjourney1.a3868b90.webp"
+  },
+  {
+    "revision": "2229037199cfaba90db996a41df48607",
+    "url": "/static/media/artjourney2.22290371.webp"
+  },
+  {
+    "revision": "9292ede95d285f07b3acd283651fd5f1",
+    "url": "/static/media/artjourney2.9292ede9.webp"
+  },
+  {
+    "revision": "4c13cf0e8938b89fb7450bfecc51278c",
+    "url": "/static/media/baliskin1.4c13cf0e.webp"
+  },
+  {
+    "revision": "60d330fdb2fc70fddd9a2ed1002646e7",
+    "url": "/static/media/baliskin1.60d330fd.webp"
+  },
+  {
+    "revision": "0f47dcd01da7c5426bfe9992aba17a00",
+    "url": "/static/media/baliskin2.0f47dcd0.webp"
+  },
+  {
+    "revision": "bdcd702193d5e5f153078cb4f1c75beb",
+    "url": "/static/media/baliskin2.bdcd7021.webp"
+  },
+  {
+    "revision": "4e94cdf952565fd13758802acc405577",
+    "url": "/static/media/cj1.4e94cdf9.webp"
+  },
+  {
+    "revision": "a28f43deda48c120ecd25029b0500245",
+    "url": "/static/media/cj1.a28f43de.webp"
+  },
+  {
+    "revision": "408f3aff911d09b7072d26ebf1bf24e0",
+    "url": "/static/media/cj2.408f3aff.webp"
+  },
+  {
+    "revision": "b4b2c97acd2a099209e450f474e86a0d",
+    "url": "/static/media/cj2.b4b2c97a.webp"
+  },
+  {
+    "revision": "8c0a69c0b95ff00bcc3af49bd90a0007",
+    "url": "/static/media/dietlogue1.8c0a69c0.webp"
+  },
+  {
+    "revision": "b3876beaf00508e229aa074e137337dc",
+    "url": "/static/media/dietlogue1.b3876bea.webp"
+  },
+  {
+    "revision": "2d3ff03817ddb5b5130a4596732c365c",
+    "url": "/static/media/dietlogue2.2d3ff038.webp"
+  },
+  {
+    "revision": "a65b438b4a57994a459ce7880cd1e6ba",
+    "url": "/static/media/dietlogue2.a65b438b.webp"
+  },
+  {
+    "revision": "dab43cee68816f65fe0743a49c83eee8",
+    "url": "/static/media/rs1.dab43cee.webp"
+  },
+  {
+    "revision": "e32b73d75528ab36cb80bd06aa46adc4",
+    "url": "/static/media/rs1.e32b73d7.webp"
+  },
+  {
+    "revision": "1116f4a45eec95616b4248dcb426d4f6",
+    "url": "/static/media/rs2.1116f4a4.webp"
+  },
+  {
+    "revision": "38ade3e0fff3c2108ed8dce32304624e",
+    "url": "/static/media/rs2.38ade3e0.webp"
+  },
+  {
+    "revision": "531a566a408a4b07dd27d27ef5aa9d25",
+    "url": "/static/media/tetris1.531a566a.webp"
+  },
+  {
+    "revision": "ea607d2288dcd0a2059f2b09cef2ad83",
+    "url": "/static/media/tetris1.ea607d22.webp"
+  },
+  {
+    "revision": "00bf1bc80023c1059aacca82997a8758",
+    "url": "/static/media/tetris2.00bf1bc8.webp"
+  },
+  {
+    "revision": "8df9d1cef3efccdb5a01dfd21a85b55d",
+    "url": "/static/media/tetris2.8df9d1ce.webp"
+  },
+  {
+    "revision": "85b7a57173bcde8c828e9b8c9ce13102",
+    "url": "/static/media/travelr1.85b7a571.webp"
+  },
+  {
+    "revision": "ca3e853f92941af006666557992f747b",
+    "url": "/static/media/travelr1.ca3e853f.webp"
+  },
+  {
+    "revision": "acbe51ade0c38d07515eab36cee9eafc",
+    "url": "/static/media/travelr2.acbe51ad.webp"
+  },
+  {
+    "revision": "f6cf0a4edeb70a754836753d0f47a5d7",
+    "url": "/static/media/travelr2.f6cf0a4e.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2504edba712df28844d4a67a0e8817f7.js"
+  "/precache-manifest.a08902fa649f5d5e3a3933844084fe83.js"
 );
 
 self.addEventListener('message', (event) => {
