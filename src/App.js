1ead9e4d1747060608c16c144a5dee06464f0022
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-04-13 12:30:22 +0100

    add about section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.scss'
 import Intro from './components/Intro'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
   return (
     <>
       <Intro />
       <Projects />
+      <About />
       <Footer />
     </>
   )
---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const Projects = () => {
+  return (
+    <section className='project-block' id='projects'>
+      <div className='is-size-2 has-text-centered is-underlined'>About me:</div>
+      <div className='columns'>
+        <div className='column has-text-centered'>
+          <h4 className='is-size-4 has-text-centered'>Experience</h4>
+          <h5>Finance Manager - May 16 - Dec 20</h5>
+          <h5>Finance Manager</h5>
+          <h6>May 16 - Dec 20</h6>
+          <h5>Finance Manager</h5>
+          <h6>May 16 - Dec 20</h6>
+          <h5>Finance Manager</h5>
+          <h6>May 16 - Dec 20</h6>
+          <h4 className='is-size-4 has-text-centered'>Education</h4>
+          <h5>General Assembly</h5>
+          <h6>Dec 19 - Mar 20</h6>
+          <h5>Finance Manager</h5>
+          <h6>May 16 - Dec 20</h6>
+          <h5>Finance Manager</h5>
+          <h6>May 16 - Dec 20</h6>
+          <h5>Finance Manager</h5>
+          <h6>May 16 - Dec 20</h6>
+        </div>
+        <div className='column description'>section right</div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
